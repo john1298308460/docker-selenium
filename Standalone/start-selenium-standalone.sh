b97c20fec295161b975405d5418563917c490785
junhong <junhong.zhou@fit2cloud.com>
2022-03-01 13:39:02 +0800

    启动多个 jar 测试

--- Standalone/start-selenium-standalone.sh
+++ Standalone/start-selenium-standalone.sh
@@ -2,6 +2,9 @@
 #
 # IMPORTANT: Change this file only in directory Standalone!
 
+#chrome
+echo "chrome" > /opt/selenium/browser_name
+
 if [ ! -z "$SE_OPTS" ]; then
   echo "Appending Selenium options: ${SE_OPTS}"
 fi
@@ -11,7 +14,50 @@ fi
 echo "Selenium Grid Standalone configuration: "
 cat /opt/selenium/config.toml
 echo "Starting Selenium Grid Standalone..."
-java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar standalone \
+nohup java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar standalone \
   --bind-host ${SE_BIND_HOST} \
   --config /opt/selenium/config.toml \
-  ${SE_OPTS}
+  --server.port=6900
+  ${SE_OPTS} &
+
+sleep 3
+
+
+#edge
+echo "MicrosoftEdge" > /opt/selenium/browser_name
+
+if [ ! -z "$SE_OPTS" ]; then
+  echo "Appending Selenium options: ${SE_OPTS}"
+fi
+
+/opt/bin/generate_config
+
+echo "Selenium Grid Standalone configuration: "
+cat /opt/selenium/config.toml
+echo "Starting Selenium Grid Standalone..."
+nohup java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar standalone \
+  --bind-host ${SE_BIND_HOST} \
+  --config /opt/selenium/config.toml \
+  --server.port=6901
+  ${SE_OPTS} &
+
+sleep 3
+
+
+#firefox
+echo "firefox" > /opt/selenium/browser_name
+
+if [ ! -z "$SE_OPTS" ]; then
+  echo "Appending Selenium options: ${SE_OPTS}"
+fi
+
+/opt/bin/generate_config
+
+echo "Selenium Grid Standalone configuration: "
+cat /opt/selenium/config.toml
+echo "Starting Selenium Grid Standalone..."
+nohup java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar standalone \
+  --bind-host ${SE_BIND_HOST} \
+  --config /opt/selenium/config.toml \
+  --server.port=6902
+  ${SE_OPTS} &
